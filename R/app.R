e13cdbc0b4b0c656593a076ce1cfb1a30fb0b0b3
KrzysztofSolowiej <krsolowiej@protonmail.com>
2023-11-04 19:09:02 +0100

    langApp

---
+++ R/app.R
@@ -0,0 +1,88 @@
+library(readr)
+library(dplyr)
+library(purrr)
+library(usethis)
+library(devtools)
+library(testthat)
+library(shiny)
+library(plotly)
+library(leaflet)
+library(DT)
+
+# setwd("C:/Users/Balon1/Desktop/IT/R_tutorial_scripts")
+
+# Add data
+
+# use_data(data, overwrite = TRUE)
+# dataset <- load("./data/data.rda")
+
+
+# Functions ---------------------------
+langApp <- function(...) {
+
+data <- read_csv("./dataset/endangered_languages.csv")
+  
+# Define UI 
+ui <- fluidPage(
+  titlePanel("Endangered languages table"),
+  sidebarPanel(
+    
+    checkboxGroupInput(
+      inputId = "degree",
+      label = "Select degree of endangerment", 
+      choices = unique(data$`Degree of endangerment`),
+      selected = unique(data$`Degree of endangerment`),
+      inline = TRUE
+    ),
+    
+    sliderInput('speak_num',  'Select min number of speakers',
+                min = 0, max = 7500000, value = 0
+    )
+  ),
+  tabsetPanel(id='my_tabsetPanel',
+              tabPanel('Table',
+                       DT::DTOutput('lang_table') 
+              ),
+              tabPanel('Map',
+                       leafletOutput('map')   
+              )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$lang_table <- DT::renderDT({
+    data %>% 
+      filter(`Degree of endangerment` == input$degree) %>%
+      filter(`Number of speakers` >= input$speak_num)
+  })
+  
+  
+  output$map <- renderLeaflet({
+    leaflet() %>% 
+      addTiles() %>% 
+      setView(-93.65, 42.0285, zoom = 6)
+  })
+  
+  observe({
+    
+    input$my_tabsetPanel
+    
+    tab1 <- leafletProxy('map', data = data
+                         # %>% filter(`Degree of endangerment` == input$degree)
+                         # %>% filter(`Number of speakers` >= input$speak_num)
+    ) %>%
+      
+      clearMarkers() %>% 
+      addCircleMarkers(lng = ~Longitude, lat = ~Latitude, radius = ~sqrt(sqrt(`Number of speakers`)), popup = ~ `Description of the location`) %>%
+      addMarkers(lng = ~Longitude, lat = ~Latitude, label = ~`Name in English`)
+    
+    
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server, ...)
+}
